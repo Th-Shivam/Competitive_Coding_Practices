fb06b94326e5499635c870712d440f06e4e345b2
th-shivam <dreamyshivam01@gmail.com>
2026-07-19 23:51:14 +0530

    CP-31 -800 -8th

--- How_Much_Does_Daytona_Cost_1878A.java
+++ How_Much_Does_Daytona_Cost_1878A.java
@@ -10,43 +10,22 @@ public class How_Much_Does_Daytona_Cost_1878A {
             int target = sc.nextInt();
 
             int[] arr = new int[n];
+
             for (int i = 0; i < n; i++) {
                 arr[i] = sc.nextInt();
             }
 
-            boolean isIFound = false;
-            int i = 0;
-            while (i < n && isIFound == false) {
-                if (arr[i] == target) {
-                    isIFound = true;
-                    HashMap<Integer, Integer> mp = new HashMap<>();
-                    for (int j = i; j < n; j++) {
-                        mp.put(arr[j], mp.getOrDefault(arr[j], 0) + 1);
+            boolean found = false;
 
-                    }
-
-                    int targetfreq = mp.get(target);
-                    int otherFreq = 0;
-
-                    for (int key : mp.keySet()) {
-                        if (key != target) {
-                            otherFreq = Math.max(otherFreq, mp.get(key));
-                        }
-                    }
-                    if (targetfreq > otherFreq) {
-                        System.out.println("Yes");
-                    } else
-                        System.out.println("No");
+            for (int x : arr) {
+                if (x == target) {
+                    found = true;
+                    break;
                 }
-
-                i++;
             }
 
-            if (!isIFound) {
-                System.out.println("NO");
-            }
+            System.out.println(found ? "YES" : "NO");
         }
-
         sc.close();
     }
 }
