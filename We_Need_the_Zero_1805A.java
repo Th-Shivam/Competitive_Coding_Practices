87b46b89dd7f2150116613112bdece55a51d2fb2
th-shivam <dreamyshivam01@gmail.com>
2026-08-04 23:29:53 +0530

    CP-31 -800 -24th

---
+++ We_Need_the_Zero_1805A.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class We_Need_the_Zero_1805A {
+     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            int xor = 0;
+
+            for (int i = 0; i < n; i++) {
+                xor ^= sc.nextInt();
+            }
+
+            if (n % 2 == 1) {
+                System.out.println(xor);
+            } else {
+                if (xor == 0)
+                    System.out.println(0);
+                else
+                    System.out.println(-1);
+            }
+        }
+
+        sc.close();
+    }
+}
