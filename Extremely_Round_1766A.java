0c029a8729c03ff9e374422ec8591314765c26d1
th-shivam <dreamyshivam01@gmail.com>
2026-08-11 21:34:04 +0530

    CP-31 -800 -29th

---
+++ Extremely_Round_1766A.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Extremely_Round_1766A {
+     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            int n = sc.nextInt();
+
+            String s = String.valueOf(n);
+
+            int digits = s.length();
+            int firstDigit = s.charAt(0) - '0';
+
+            int answer = (digits - 1) * 9 + firstDigit;
+
+            System.out.println(answer);
+        }
+    }
+}
