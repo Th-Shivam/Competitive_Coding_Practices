b539e91fde68318ff667cbc2fedbeef5686245b5
th-shivam <dreamyshivam01@gmail.com>
2026-09-02 23:51:20 +0530

    CP-31 -900 -20th

---
+++ Make_it_Divisible_by_25_1593B.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class Make_it_Divisible_by_25_1593B {
+      static int minOperations(String n, String possibleValue) {
+        int operations = 0;
+        int checkerIndex = possibleValue.length() - 1;
+
+        for (int i = n.length() - 1; i >= 0; i--) {
+            if (n.charAt(i) == possibleValue.charAt(checkerIndex)) {
+                checkerIndex--;
+
+                if (checkerIndex < 0)
+                    break;
+            } else {
+                operations++;
+            }
+        }
+
+        if (checkerIndex >= 0)
+            operations = Integer.MAX_VALUE;
+
+        return operations;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int tt = sc.nextInt();
+
+        while (tt-- > 0) {
+            String n = sc.next();
+
+            String[] possibleValues = {"00", "25", "50", "75"};
+
+            int ans = Integer.MAX_VALUE;
+
+            for (String possibleValue : possibleValues) {
+                ans = Math.min(ans, minOperations(n, possibleValue));
+            }
+
+            System.out.println(ans);
+        }
+
+        sc.close();
+    }
+}
