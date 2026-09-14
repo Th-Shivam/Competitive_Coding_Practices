6817bf34f65e3d22111e791253454f0423fb5974
th-shivam <dreamyshivam01@gmail.com>
2026-09-14 23:37:22 +0530

    CP-31 -1000 1st

---
+++ Swap_and_Delete_1913B.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class Swap_and_Delete_1913B {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        if (!sc.hasNextInt()) return;
+        int tt = sc.nextInt();
+        
+        while (tt-- > 0) {
+            String s = sc.next();
+            int n = s.length(); 
+            
+            int count_of_0s = 0;
+            int count_of_1s = 0;
+ 
+            for (int i = 0; i < n; i++) {
+                if (s.charAt(i) == '0') {
+                    count_of_0s++;
+                } else {
+                    count_of_1s++;
+                }
+            }
+
+            int length_of_t = 0;
+            
+            for (int i = 0; i < n; i++) {
+                if (s.charAt(i) == '0' && count_of_1s > 0) {
+                    count_of_1s--;
+                    length_of_t++;
+                } else if (s.charAt(i) == '1' && count_of_0s > 0) {
+                    count_of_0s--;
+                    length_of_t++;
+                } else {
+                    break;
+                }
+            }
+
+            System.out.println(n - length_of_t);
+        }
+        
+        sc.close();
+    }
+}
